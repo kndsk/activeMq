993efc434567c015118ee2639fe6be61dcdd8e60
kndsk <820034705@qq.com>
2018-04-19 11:28:39 +0800

    支持redis

--- task_service/src/test/java/com/whty/test/BaseTest.java
+++ task_service/src/test/java/com/whty/test/BaseTest.java
@@ -9,8 +9,8 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"classpath:/spring/*.xml","classpath:/mybatis/*.xml","classpath:/mongodb/*.xml","classpath:/solr/*.xml"})
-//@ContextConfiguration(locations = {"classpath:/spring/*.xml","classpath:/mongodb/*.xml"})
+//@ContextConfiguration(locations = {"classpath:/spring/*.xml","classpath:/mybatis/*.xml","classpath:/mongodb/*.xml","classpath:/solr/*.xml","classpath:/redis/*.xml"})
+@ContextConfiguration(locations = {"classpath:/spring/*.xml","classpath:/redis/*.xml"})
 public class BaseTest {
 	public final Logger logger = LoggerFactory
 			.getLogger(this.getClass());
